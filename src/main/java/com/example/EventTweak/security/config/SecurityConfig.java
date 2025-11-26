c1d297900d1719f235bdbc61a421fe62b2900432
AryanKesarkar2803 <aryankesarkar0033@gmail.com>
2025-11-26 22:02:13 +0530

    fix: change frontend url to prod

--- src/main/java/com/example/EventTweak/security/config/SecurityConfig.java
+++ src/main/java/com/example/EventTweak/security/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://event-tweak-client.onrender.com/"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
